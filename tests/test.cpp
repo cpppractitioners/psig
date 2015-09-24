3c0635de485b5526ccb5263c69f4f292e732e7c4
Chris Knight <cknite@knightmerica.net>
2015-09-24 00:07:35 -0500

    Fix tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,20 +4,29 @@
 #define KTL_CHECK(cond)                            \
     if (!(cond))                                   \
     {                                              \
-        std::cout << "Error: ##cond" << std::endl; \
+        std::cout << "Error: " #cond << std::endl; \
         exit(1);                                   \
     }
 
 void test_mask()
 {
-    namespace kps = psig;
+    psig::sigset emptysigset = psig::this_thread::get_mask();
+    KTL_CHECK(emptysigset.empty());
 
-    kps::sigset newsigset{SIGTERM, SIGINT};
-    kps::sigset oldsigset = kps::this_thread::set_mask(newsigset);
-    kps::sigset cursigset = kps::this_thread::get_mask();
+    psig::sigset newsigset{SIGTERM, SIGINT};
+    psig::sigset oldsigset = psig::this_thread::set_mask(newsigset);
+    psig::sigset cursigset = psig::this_thread::get_mask();
 
     std::cout << "Signals in new mask: ";
-    for (kps::signum_t signum : newsigset.signals()) std::cout << signum << " ";
+    for (psig::signum_t signum : newsigset.signals()) std::cout << signum << " ";
+    std::cout << std::endl;
+
+    std::cout << "Signals in old mask: ";
+    for (psig::signum_t signum : oldsigset.signals()) std::cout << signum << " ";
+    std::cout << std::endl;
+
+    std::cout << "Signals in cur mask: ";
+    for (psig::signum_t signum : cursigset.signals()) std::cout << signum << " ";
     std::cout << std::endl;
 
     KTL_CHECK(oldsigset.empty());
@@ -33,55 +42,49 @@ void test_mask()
     KTL_CHECK(newsigset.has(SIGINT) == false);
     KTL_CHECK(newsigset.has(SIGHUP) == false);
 
-    kps::this_thread::clear_mask();
-    cursigset = kps::this_thread::get_mask();
+    psig::this_thread::clear_mask();
+    cursigset = psig::this_thread::get_mask();
     KTL_CHECK(cursigset.empty());
     KTL_CHECK(cursigset.has(SIGTERM) == false);
     KTL_CHECK(cursigset.has(SIGINT) == false);
     KTL_CHECK(cursigset.has(SIGHUP) == false);
 
-    kps::this_thread::fill_mask();
-    cursigset = kps::this_thread::get_mask();
+    psig::this_thread::fill_mask();
+    cursigset = psig::this_thread::get_mask();
     KTL_CHECK(cursigset.empty() == false);
     KTL_CHECK(cursigset.has(SIGTERM));
     KTL_CHECK(cursigset.has(SIGINT));
     KTL_CHECK(cursigset.has(SIGHUP));
     KTL_CHECK(cursigset.has(SIGRTMIN));
     KTL_CHECK(cursigset.has(SIGRTMAX));
-    KTL_CHECK(cursigset.has(kps::rt::signum(0)));  // SIGRTMIN
-    KTL_CHECK(cursigset.has(kps::rt::signum(1)));  // SIGRTMIN+1
-    KTL_CHECK(cursigset.has(kps::rt::signum(kps::rt::sigcount())));  // SIGRTMAX
+    KTL_CHECK(cursigset.has(psig::rt::signum(0)));  // SIGRTMIN
+    KTL_CHECK(cursigset.has(psig::rt::signum(1)));  // SIGRTMIN+1
+    KTL_CHECK(cursigset.has(psig::rt::signum(psig::rt::sigcount())));  // SIGRTMAX
 }
 
 void test_wait()
 {
-    namespace kps = psig;
-
-    kps::sigset signals{SIGTERM, SIGINT, SIGHUP};
-    for (kps::sigcnt_t rtsigcnt = 0; rtsigcnt <= kps::rt::sigcount();
+    psig::sigset signals{SIGTERM, SIGINT, SIGHUP};
+    for (psig::sigcnt_t rtsigcnt = 0; rtsigcnt <= psig::rt::sigcount();
          ++rtsigcnt)
-        signals += kps::rt::signum(rtsigcnt);
+        signals += psig::rt::signum(rtsigcnt);
 
     std::cout << "Please type Ctrl^C" << std::endl;
-    const kps::signum_t signum = kps::wait(signals);
+    const psig::signum_t signum = psig::wait(signals);
     std::cout << "Received signal: " << signum << std::endl;
 }
 
 void test_rt_wait()
 {
-    namespace kps = psig;
-
     std::cout << "Please type Ctrl^C" << std::endl;
-    const kps::signum_t signum = kps::wait({SIGTERM, SIGINT, SIGHUP});
+    const psig::signum_t signum = psig::wait({SIGTERM, SIGINT, SIGHUP});
     std::cout << "Received signal: " << signum << std::endl;
 }
 
 void test_timed_wait()
 {
-    namespace kps = psig;
-
     std::cout << "Please type Ctrl^C within 2.25 seconds" << std::endl;
-    const kps::signum_t signum = kps::wait(
+    const psig::signum_t signum = psig::wait(
         {SIGTERM, SIGINT, SIGHUP}, std::chrono::nanoseconds(2250000000));
     std::cout << "Received signal: " << signum << std::endl;
 }
